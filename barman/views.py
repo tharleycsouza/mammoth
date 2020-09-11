5e4e51fa92eb9669a2c9cf22668485c395b75cef
Tharley Carvalho <tharley.carvalho@locaweb.com.br>
2020-09-10 22:02:30 -0300

    added new dashboards

--- barman/views.py
+++ barman/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def dashboard(request):
+    """Dashboard.
+    """
+    return render(
+        request,
+        "dashboard.html",
+        {
+            "nav_active": "dashboard"
+        }
+    )
